cb5754d295443dc526408256d32ac1e2ad001de7
xianyaochen <owenchenxy@gmail.com>
2024-10-18 12:36:58 +0800

    add files

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def generate_nginx_config(config):
     template_loader = jinja2.FileSystemLoader(searchpath="./templates")
     template_env = jinja2.Environment(loader=template_loader)
     template = template_env.get_template("mirrors.conf.j2")
-    output = template.render(registries=config["registries"])
+    output = template.render(config)
 
     # Write the rendered template to the nginx directory
     with open("nginx/conf.d/mirrors.conf", "w") as f:
@@ -60,7 +60,7 @@ def generate_docker_compose_config(config):
     template_loader = jinja2.FileSystemLoader(searchpath="./templates")
     template_env = jinja2.Environment(loader=template_loader)
     template = template_env.get_template("docker-compose.yml.j2")
-    output = template.render(registries=config["registries"])
+    output = template.render(config)
 
     # Write the rendered template to the docker-compose directory
     with open("docker-compose.yml", "w") as f:
